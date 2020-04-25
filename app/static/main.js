e82e7e79e24e178ce8360d41ffeca0688cd10eb7
angelonoob <amatirta.angelo@gmail.com>
2020-04-25 23:49:47 +0800

    add more function like try again and changing location

--- app/static/main.js
+++ app/static/main.js
@@ -1,27 +1,43 @@
 $(document).ready(function () {
     console.log("ready!");
-
+    $('#try-again').hide();
+    
     $("form").on("submit", function () {
         console.log("Form submitted")
-
+        
         var valueOne = $('input[name="location"]').val()
         var valueTwo = $('input[name="language"]').val()
         console.log(valueOne, valueTwo)
-
+        
         $.ajax({
             type: "POST",
             url: "/",
             data: { first: valueOne, second: valueTwo },
             success: function (results) {
-                console.log(results.items[0]);
-                $("#results").html('<a href="'+results.items[0].html_url+'">'+results.items[0].login+
-                '</a><br><img src="'+results.items[0].avatar_url+'">')
-                $("input").val("")
+                if (results.items.length > 0){
+                    $('input').hide();
+                    $('#try-again').show();
+                    // generate random object
+                    var randNum = Math.floor(Math.random() * Object.keys(results.items).length)
+                    console.log(results.items[randNum]);
+                    $("#results").html('<a href="'+results.items[randNum].html_url+'">'+results.items[randNum].login+
+                    '</a><br><img src="'+results.items[randNum].avatar_url+'" class="avatar">')
+                    // $("input").val("")
+                } else {
+                    $('#results').html('Oops.. Please try again.')
+                    $("input").val("")
+                }
             },
             error: function (error) {
                 console.log(error);
             }
         });
+    });
+    
+    $('#try-again').on('click', function(){
+        $('input').val('').show();
+        $('#try-again').hide();
+        $('#results').html('');        
     });
 
 });
