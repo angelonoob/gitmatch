3b0fdcd4e1f1156824872655073318d6fb675189
angelonoob <amatirta.angelo@gmail.com>
2020-04-26 14:35:50 +0800

    add cache

--- app/app.py
+++ app/app.py
@@ -1,22 +1,11 @@
+import requests, requests_cache, tempfile
+
 from flask import Flask, render_template, request, jsonify
-import requests
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "hello weird"
+requests_cache.install_cache('{}/cache'.format(tempfile.gettempdir()),expire_after=180)
 
-
-# @app.route('/', methods=['GET', 'POST'])
-# def home():
-#     if request.method == 'POST':
-#         value_one = int(request.form.get('first'))
-#         value_two = int(request.form.get('second'))
-#         total = value_one + value_two
-#         data = {"total": str(total)}
-#         return jsonify(data)
-#     return render_template('index.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -27,6 +16,7 @@ def home():
         # api call
         url = 'https://api.github.com/search/users?q=location:{0}+language:{1}'.format(value_one, value_two)
         respons_dict = requests.get(url).json()
+        # return json
         return jsonify(respons_dict)
     return render_template('index.html')
 
